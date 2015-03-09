152ae1a3d03f34243fc7343e4021a2987be039ef
Martin Hagstrom <marhag87@gmail.com>
2015-03-09 18:57:46 +0100

    Convert to jquery syntax

--- client/client.js
+++ client/client.js
@@ -27,12 +27,12 @@ var skills = [{name: "athletics",       attribute: "str"},
               {name: "persuasion",      attribute: "cha"}];
 
 function update_proficiency(field, attribute) {
-  var attribute_mod = document.getElementById(attribute + "_mod").value;
-  var proficient_skill = document.getElementById(field).checked;
+  var attribute_mod = $("#" + attribute + "_mod").val();
+  var proficient_skill = $("#" + field).prop('checked');
   var skill_bonus = Number(attribute_mod);
   if (proficient_skill)
     skill_bonus += Number(proficiency_bonus);
-  document.getElementById(field + "_bonus").value = prepend_plus(skill_bonus);
+  $("#" + field + "_bonus").val(prepend_plus(skill_bonus));
 }
 
 function prepend_plus(variable) {
@@ -44,64 +44,64 @@ function prepend_plus(variable) {
 function update_character_DB() {
   // Update character in DB
   Characters.update({_id: "jzZfNjRecszsFHQ67"},{$set: {
-    character_name:          document.getElementById("character_name").value,
-    race:                    document.getElementById("race").value,
-    subrace:                 document.getElementById("subrace").value,
-    alignment:               document.getElementById("alignment").value,
-    background:              document.getElementById("background").value,
-    player_name:             document.getElementById("player_name").value,
-    class:                   document.getElementById("class").value,
-    subclass:                document.getElementById("subclass").value,
-    experience_points:       document.getElementById("experience_points").value,
-    str:                     document.getElementById("str").value,
-    dex:                     document.getElementById("dex").value,
-    con:                     document.getElementById("con").value,
-    int:                     document.getElementById("int").value,
-    wis:                     document.getElementById("wis").value,
-    cha:                     document.getElementById("cha").value,
-    inspiration:             document.getElementById("inspiration").value,
-    str_save:                document.getElementById("str_save").checked,
-    dex_save:                document.getElementById("dex_save").checked,
-    con_save:                document.getElementById("con_save").checked,
-    int_save:                document.getElementById("int_save").checked,
-    wis_save:                document.getElementById("wis_save").checked,
-    cha_save:                document.getElementById("cha_save").checked,
-    acrobatics_skill:        document.getElementById("acrobatics_skill").checked,
-    animal_handling_skill:   document.getElementById("animal_handling_skill").checked,
-    arcana_skill:            document.getElementById("arcana_skill").checked,
-    athletics_skill:         document.getElementById("athletics_skill").checked,
-    deception_skill:         document.getElementById("deception_skill").checked,
-    history_skill:           document.getElementById("history_skill").checked,
-    insight_skill:           document.getElementById("insight_skill").checked,
-    intimidation_skill:      document.getElementById("intimidation_skill").checked,
-    investigation_skill:     document.getElementById("investigation_skill").checked,
-    medicine_skill:          document.getElementById("medicine_skill").checked,
-    nature_skill:            document.getElementById("nature_skill").checked,
-    perception_skill:        document.getElementById("perception_skill").checked,
-    performance_skill:       document.getElementById("performance_skill").checked,
-    persuasion_skill:        document.getElementById("persuasion_skill").checked,
-    religion_skill:          document.getElementById("religion_skill").checked,
-    sleight_of_hand_skill:   document.getElementById("sleight_of_hand_skill").checked,
-    stealth_skill:           document.getElementById("stealth_skill").checked,
-    survival_skill:          document.getElementById("survival_skill").checked,
-    other_proficiencies:     document.getElementById("other_proficiencies").value,
-    max_hitpoints_from_roll: document.getElementById("max_hitpoints_from_roll").value,
-    current_hitpoints:       document.getElementById("current_hitpoints").value,
-    temporary_hitpoints:     document.getElementById("temporary_hitpoints").value,
-    hit_dice_current:        document.getElementById("hit_dice_current").value,
-    death_saves_successes_1: document.getElementById("death_saves_successes_1").checked,
-    death_saves_successes_2: document.getElementById("death_saves_successes_2").checked,
-    death_saves_successes_3: document.getElementById("death_saves_successes_3").checked,
-    death_saves_failures_1:  document.getElementById("death_saves_failures_1").checked,
-    death_saves_failures_2:  document.getElementById("death_saves_failures_2").checked,
-    death_saves_failures_3:  document.getElementById("death_saves_failures_3").checked,
-    spells:                  document.getElementById("spells").value,
-    cp:                      document.getElementById("cp").value,
-    sp:                      document.getElementById("sp").value,
-    ep:                      document.getElementById("ep").value,
-    gp:                      document.getElementById("gp").value,
-    pp:                      document.getElementById("pp").value,
-    features_and_traits:     document.getElementById("features_and_traits").value,
+    character_name:          $("#character_name").val(),
+    race:                    $("#race").val(),
+    subrace:                 $("#subrace").val(),
+    alignment:               $("#alignment").val(),
+    background:              $("#background").val(),
+    player_name:             $("#player_name").val(),
+    class:                   $("#class").val(),
+    subclass:                $("#subclass").val(),
+    experience_points:       $("#experience_points").val(),
+    str:                     $("#str").val(),
+    dex:                     $("#dex").val(),
+    con:                     $("#con").val(),
+    int:                     $("#int").val(),
+    wis:                     $("#wis").val(),
+    cha:                     $("#cha").val(),
+    inspiration:             $("#inspiration").val(),
+    str_save:                $("#str_save").prop('checked'),
+    dex_save:                $("#dex_save").prop('checked'),
+    con_save:                $("#con_save").prop('checked'),
+    int_save:                $("#int_save").prop('checked'),
+    wis_save:                $("#wis_save").prop('checked'),
+    cha_save:                $("#cha_save").prop('checked'),
+    acrobatics_skill:        $("#acrobatics_skill").prop('checked'),
+    animal_handling_skill:   $("#animal_handling_skill").prop('checked'),
+    arcana_skill:            $("#arcana_skill").prop('checked'),
+    athletics_skill:         $("#athletics_skill").prop('checked'),
+    deception_skill:         $("#deception_skill").prop('checked'),
+    history_skill:           $("#history_skill").prop('checked'),
+    insight_skill:           $("#insight_skill").prop('checked'),
+    intimidation_skill:      $("#intimidation_skill").prop('checked'),
+    investigation_skill:     $("#investigation_skill").prop('checked'),
+    medicine_skill:          $("#medicine_skill").prop('checked'),
+    nature_skill:            $("#nature_skill").prop('checked'),
+    perception_skill:        $("#perception_skill").prop('checked'),
+    performance_skill:       $("#performance_skill").prop('checked'),
+    persuasion_skill:        $("#persuasion_skill").prop('checked'),
+    religion_skill:          $("#religion_skill").prop('checked'),
+    sleight_of_hand_skill:   $("#sleight_of_hand_skill").prop('checked'),
+    stealth_skill:           $("#stealth_skill").prop('checked'),
+    survival_skill:          $("#survival_skill").prop('checked'),
+    other_proficiencies:     $("#other_proficiencies").val(),
+    max_hitpoints_from_roll: $("#max_hitpoints_from_roll").val(),
+    current_hitpoints:       $("#current_hitpoints").val(),
+    temporary_hitpoints:     $("#temporary_hitpoints").val(),
+    hit_dice_current:        $("#hit_dice_current").val(),
+    death_saves_successes_1: $("#death_saves_successes_1").prop('checked'),
+    death_saves_successes_2: $("#death_saves_successes_2").prop('checked'),
+    death_saves_successes_3: $("#death_saves_successes_3").prop('checked'),
+    death_saves_failures_1:  $("#death_saves_failures_1").prop('checked'),
+    death_saves_failures_2:  $("#death_saves_failures_2").prop('checked'),
+    death_saves_failures_3:  $("#death_saves_failures_3").prop('checked'),
+    spells:                  $("#spells").val(),
+    cp:                      $("#cp").val(),
+    sp:                      $("#sp").val(),
+    ep:                      $("#ep").val(),
+    gp:                      $("#gp").val(),
+    pp:                      $("#pp").val(),
+    features_and_traits:     $("#features_and_traits").val(),
   }});
 }
 
@@ -110,7 +110,7 @@ function update_forms(){
   attributes.forEach(function (attribute) {
     var attribute_score = document.getElementById(attribute);
     var attribute_mod = Math.floor((Number(attribute_score.value) - 10) / 2);
-    document.getElementById(attribute + "_mod").value = prepend_plus(attribute_mod);
+    $("#" + attribute + "_mod").val(prepend_plus(attribute_mod));
   });
 
   // Update experience points
@@ -129,11 +129,11 @@ function update_forms(){
     current_level = exp_per_level.length - index;
     return Number(current_exp.value) >= levelrange;
   });
-  document.getElementById("level").value = current_level;
+  $("#level").val(current_level);
 
   // Set proficiency bonus
   proficiency_bonus = (Math.floor((Number(current_level) - 1) / 4)) + 2;
-  document.getElementById("proficiency_bonus").value = proficiency_bonus;
+  $("#proficiency_bonus").val(proficiency_bonus);
 
   // Set saving throws bonuses
   attributes.forEach(function (attribute) {
@@ -146,39 +146,39 @@ function update_forms(){
   });
 
   // Set passive wisdom
-  document.getElementById("passive_wisdom").value = 10 + Number(document.getElementById("perception_skill_bonus").value);
+  $("#passive_wisdom").value = 10 + Number($("#perception_skill_bonus").val());
 
   // Set armor class
   // TODO: Take into account gear/skills. Make writable but keep suggestion?
-  document.getElementById("armor_class").value = 10 + Number(document.getElementById("dex_mod").value);
+  $("#armor_class").value = 10 + Number($("#dex_mod").val());
 
   // Set initiative
   // TODO: Take into account class, race, other features.
-  document.getElementById("initiative").value = Number(document.getElementById("dex_mod").value);
+  $("#initiative").value = Number($("#dex_mod").val());
 
   // Set speed
-  var race_data = Races.findOne({name: document.getElementById("race").value});
+  var race_data = Races.findOne({name: $("#race").val()});
   if (typeof race_data !== 'undefined') {
-    document.getElementById("speed").value = race_data.speed;
+    $("#speed").val(race_data.speed);
   }
 
   // Set max hp
-  var class_data = Classes.findOne({name: document.getElementById("class").value});
+  var class_data = Classes.findOne({name: $("#class").val()});
   if (typeof class_data !== 'undefined') {
-    document.getElementById("max_hitpoints_from_level").value = class_data.hp;
-    document.getElementById("max_hitpoints").value = Number(document.getElementById("max_hitpoints_from_level").value) + Number(document.getElementById("max_hitpoints_from_roll").value)
+    $("#max_hitpoints_from_level").val(class_data.hp);
+    $("#max_hitpoints").val(Number($("#max_hitpoints_from_level").val()) + Number($("#max_hitpoints_from_roll").val()));
   }
 
   // Set hit dice
-  if (typeof document.getElementById("level").value !== 'undefined' && typeof class_data !== 'undefined') {
-    document.getElementById("hit_dice_total").value = document.getElementById("level").value + "d" + class_data.hp;
+  if (typeof $("#level").val() !== 'undefined' && typeof class_data !== 'undefined') {
+    $("#hit_dice_total").val($("#level").val() + "d" + class_data.hp);
   }
 
   // Set weapon info
-  var weapon_data = Weapons.findOne({name: document.getElementById("weapons").value});
+  var weapon_data = Weapons.findOne({name: $("#weapons").val()});
   if (typeof weapon_data !== 'undefined') {
-    document.getElementById("attacks_and_spellcasting_weapons_attack_bonus").value = document.getElementById(weapon_data.mod + "_save_bonus").value;
-    document.getElementById("attacks_and_spellcasting_weapons_damage").value = weapon_data.damage + " " + weapon_data.damage_type;
+    $("#attacks_and_spellcasting_weapons_attack_bonus").val($("#" + weapon_data.mod + "_save_bonus").val());
+    $("#attacks_and_spellcasting_weapons_damage").val(weapon_data.damage + " " + weapon_data.damage_type);
   }
 
   // Set weapon attack bonus
@@ -211,8 +211,8 @@ Template.character.helpers({
   },
   subclasses: function () {
     // TODO: Make this dynamic
-    //if (typeof document.getElementById("class").value !== 'undefined') {
-    //  return Classes.find({name: document.getElementById("class").value});
+    //if (typeof $("class").val() !== 'undefined') {
+    //  return Classes.find({name: $("class").val()});
     //}
     return Classes.find({name: "Barbarian"});
   },
@@ -231,10 +231,10 @@ setTimeout(function () {
   // TODO: Make this happen on callback/reactively
   var character_data = Characters.findOne({_id: "jzZfNjRecszsFHQ67"});
   if (typeof character_data !== 'undefined') {
-   document.getElementById("race").value = character_data.race
-   document.getElementById("class").value = character_data.class
-   document.getElementById("subclass").value = character_data.subclass
-   document.getElementById("subrace").value = character_data.subrace
+   $("#race").val(character_data.race)
+   $("#class").val(character_data.class)
+   $("#subclass").val(character_data.subclass)
+   $("#subrace").val(character_data.subrace)
   }
   update_forms();
 }, 1000);
